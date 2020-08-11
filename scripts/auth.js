a7bcb7f2afb5b496c445a683e41ba98e1a3122b5
pjain312 <prachijain3102@gmail.com>
2020-08-11 14:50:44 +0530

    "Saving thr state"

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,11 @@
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('user logged in: ', user);
+  } else {
+    console.log('user logged out');
+  }
+});
+
 
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
