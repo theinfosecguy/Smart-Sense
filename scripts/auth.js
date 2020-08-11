fd699b993e9abae55b4ce37a0dcc45df42248439
pjain312 <prachijain3102@gmail.com>
2020-08-11 14:26:17 +0530

    "User Login"

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,18 +1,19 @@
 
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
 
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#exampleModal');
+    // M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
 
-    auth.SignInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-
-        // close the login modal and reset it
-
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    })
-})
+});
