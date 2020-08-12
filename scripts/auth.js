f0ced5ba8c804e0f1d5fd909dbb12208ff5c33b6
pjain312 <prachijain3102@gmail.com>
2020-08-13 01:23:57 +0530

    "final"

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,21 +6,19 @@ auth.onAuthStateChanged(user => {
 });
 
 
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+
 
   // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
-    // close the signup modal & reset form
-    const modal = document.querySelector('#exampleModal');
-    // M.Modal.getInstance(modal).close();
-    loginForm.reset();
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(function() {
+          const loginForm = document.querySelector('#login-form');
+          loginForm.addEventListener('submit', (e) => {
+          e.preventDefault();
+        
+        // get user info
+          const email = loginForm['login-email'].value;
+          const password = loginForm['login-password'].value;
+          return firebase.auth().signInWithEmailAndPassword(email, password);
   });
 
 });
